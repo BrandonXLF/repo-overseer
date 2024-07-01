21e33895382b2d3f3c3ad411a9d87497e2424133
Brandon Fowler <brandfowl@gmail.com>
2024-07-01 04:24:29 -0400

    Add logo

--- src/Title.tsx
+++ src/Title.tsx
@@ -3,7 +3,7 @@ import './Title.css';
 export default function Title() {
 	return <hgroup>
 		<h1>
-			<div className="css-logo" />
+			<img src="logo-half.png" className="logo" alt="Repo Overseer logo" />
 			<span>Repo Overseer</span>
 		</h1>
 		<p>View issues and pull requests in all your GitHub repositories.</p>
