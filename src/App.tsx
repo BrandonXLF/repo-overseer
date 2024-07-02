9dc7f46506e90e63cca926427c0d167107033426
Brandon Fowler <brandfowl@gmail.com>
2024-07-01 22:49:16 -0400

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import IssueList from './IssueList';
 import Title from './Title';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
 	return (
 		<main>
 			<Title />
 			<IssueList />
+			<Footer />
 		</main>
 	);
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,8 @@
+import './Footer.css';
+
+export default function Footer() {
+	return <footer>
+		Made by <a href="https://github.com/BrandonXLF">Brandon Fowler</a>.{' '}
+		<a href="https://github.com/BrandonXLF/repo-overseer">View source code</a>.
+	</footer>;
+}
