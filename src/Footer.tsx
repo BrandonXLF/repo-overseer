886f554af7ef6ee5859c866c3b82573b0e243dc6
Brandon Fowler <42590338+BrandonXLF@users.noreply.github.com>
2024-08-06 18:21:01 -0400

    Remove extra space

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,8 +4,7 @@ export default function Footer() {
 	return (
 		<footer>
 			Made by <a href="https://github.com/BrandonXLF">Brandon Fowler</a>.{' '}
-			<a href="https://github.com/BrandonXLF/repo-overseer">View source code</a>{' '}
-			.
+			<a href="https://github.com/BrandonXLF/repo-overseer">View source code</a>{' '}.
 		</footer>
 	);
 }
