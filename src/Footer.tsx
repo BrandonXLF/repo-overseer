fad526ab1e63c734262c1f4ea275294a5f60e0f4
Brandon Fowler <brandfowl@gmail.com>
2025-08-06 21:20:27 -0400

    Run prettier

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,8 @@ export default function Footer() {
 	return (
 		<footer>
 			Made by <a href="https://github.com/BrandonXLF">Brandon Fowler</a>.{' '}
-			<a href="https://github.com/BrandonXLF/repo-overseer">View source code</a>{' '}.
+			<a href="https://github.com/BrandonXLF/repo-overseer">View source code</a>{' '}
+			.
 		</footer>
 	);
 }
